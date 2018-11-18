1861a14f26a90a85d65a7d19b6d6f9523e766f1b
Alex24432 <ftzxly@126.com>
2018-11-18 14:04:11 +0800

    add a js file

---
+++ JavaScript2.js
@@ -0,0 +1,11 @@
+﻿window.onload = function () {
+    var oTxt1 = document.getElementById('txt1');
+    var oTxt2 = document.getElementById('txt2');
+    var oBtn = document.getElementById('btn1');
+
+    oBtn.onclick = function () { 
+        alert(parseInt(oTxt1.value) + parseInt(oTxt2.value));
+    
+    };
+
+}
